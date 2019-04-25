f689ca7e95be53c0443aaae3500c450d862044e0
jinkstudy <sweetyks486@gmail.com>
2019-04-25 16:03:35 +0900

    no message

--- OOP/src/practice0425/Main.java
+++ OOP/src/practice0425/Main.java
@@ -2,20 +2,24 @@ package practice0425;
 
 import java.util.Scanner;
 
-import h_inherit_book.Item;
 
 public class Main {
 
 	public static void main(String[] args) {
+		do {
 		System.out.println("언어를 선택하세요(1.한국어 2.영어  3.일본어)");
 		Scanner input = new Scanner(System.in);
 		
+		// 부모 class 변수 선언
 		Language lang= null;
 		char answer;
 		
 		int choice1 = input.nextInt();
 		input.nextLine();
-		do {
+		
+		//대답이 y이면 계속 반복하도록.
+		
+			//선택 옵션에 따라 어떤 자식클래스의 객체를 생성할지.
 			switch(choice1){
 			case 1: lang = new Kor(); break;
 			case 2: lang = new Eng(); break;
@@ -26,11 +30,14 @@ public class Main {
 			
 			int choice2 = input.nextInt();
 			
+			//선택 옵션에 따라 어떤 메소드를 생성할지.
 			switch(choice2) {
 			case 1 : lang.greeting(); break;
 			case 2 : lang.selfIntro(); break;
 			case 3 : lang.saying(); break;
 			}
+			
+			// 다시할 것인지 확인.
 			
 			System.out.println("다시 하시겠습니까?(Y/N)");
 			 answer = input.next().charAt(0);
