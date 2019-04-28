bc08880ed81abe8d9cc25748871d840526f44d41
jinkstudy <sweetyks486@gmail.com>
2019-04-28 22:23:36 +0900

    T

--- OOP/src/practice0426_food/Main.java
+++ OOP/src/practice0426_food/Main.java
@@ -7,21 +7,37 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("메뉴를 확인하세요");
 		
+		//PizzaStore 변수, 객체생성
 		PizzaStore order=new PizzaStore();
+		System.out.println();System.out.println();
+		//사용자 입력받기
 		Scanner input = new Scanner(System.in);
-		System.out.println("메뉴를 선택하세요 ex)파전,막걸리");
+		System.out.println("메뉴를 선택하세요 ex)치즈후라이피자,고구마피자");
 		String menu=input.next();
+		//입력받은 내용 분리하여 orderlist 배열에 저장.
 		StringTokenizer sr = new StringTokenizer(menu,",");
-		
-		String[] orderlist= new String[5];
+
+		String[] orderlist= new String[sr.countTokens()];//sr.countTokens() Token갯수 반환해주는 메소드.
 		for (int i = 0; sr.hasMoreTokens(); i++) {
 			orderlist[i] = sr.nextToken();
 		}
+
+		System.out.println();
+		
+		//orderlist를 setOrderList의 매개변수로 저장.수행
 		
 		order.setOrderList(orderlist);
+		
+		//getTotalPrice 메소드 수행 반환 값 total변수에 저장.
 		int total = order.getTotalPrice();
-		System.out.println("총 금액은 " + total);
-	
+		System.out.println("선택한 메뉴를 확인하세요");
+
+		for (int i = 0; i < orderlist.length; i++) {
+			System.out.println(orderlist[i] + " " );
+		}
+		System.out.println();
+		System.out.println("총 금액은 " + total+"원 입니다.");
+
 
 	}
 
--- OOP/src/practice0426_food/PizzaStore.java
+++ OOP/src/practice0426_food/PizzaStore.java
@@ -1,72 +1,60 @@
 package practice0426_food;
 
-import java.util.Scanner;
-import java.util.StringTokenizer;
-
-import c_array.Student;
-
+//DeliveryStore 인터페이스 구현.
 public class PizzaStore implements DeliveryStore {
 
-	Food[] food =new Food[7];
-	int n;
-	String[] orderList=null;
+	private Food[] food =new Food[7];//Menu 갯수만큼 초기화.
+	private String[] orderList; 
 
+	//PizzaStore 생성자 실행 시 메뉴 및 가격 초기화.
 	public PizzaStore(){
 
-		String[] menu_list = {"김치찌개","된장찌개","파전","불고기","소주","맥주","막걸리"};
-		int[]  price = {5000,5000,10000,15000,4000,4000,5000};
+		String[] menu_list = {"치즈후라이피자","고구마피자","불닭피자","오븐파스타","치킨윙","생맥주","콜라"};
+		int[]  price = {13000,14000,10000,8000,6000,7000,2000};
 
 		for (int i = 0; i <7; i++) {
-			food[i] = new Food();
+			food[i] = new Food(); //Food배열 안에서 각각 다시  menu_list와  price 배열로 구성되므로 초기화 해줘야한다. 까먹지말것!!!
 			food[i].name = menu_list[i];
 			food[i].price = price[i];
-			System.out.print(food[i].name+",");
+
+			//메뉴 및 가격 확인용
+			System.out.println(food[i].name+ "      "+ food[i].price);
 		}
-
 	}
-
-
-
+	//메인에서 사용자에게 입력받은 orderList를 매개변수로 받아서 클래스의 orderList 변수에 저장.
 	public void setOrderList(String[] orderList) {
-	
-		for (int i = 0; i < orderList.length; i++) {
-			this.orderList[i]=orderList[i];
-		}
-	}
 
+		this.orderList=new String[orderList.length]; 
+		
+		//최대 주문가능 수량 5개까지 조건.
+		if(orderList.length <6){
+			for (int i = 0; i < orderList.length; i++) {
+				this.orderList[i]=orderList[i];
+				//System.out.println(this.orderList[i]); //확인용
+			}
+		}else {
+			System.out.println("최대 5개까지만 주문가능합니다. 다시 시작하세요");
+			System.exit(0);
+		}
+
+	}
+	//orderList에 있는 음식들의 가격을 합해서 총 가격 구하기.
 	public int getTotalPrice(){
 		int total = 0;
-		int[]price=new int[n];
+		int[]price=new int[orderList.length];
 
-		for (int j = 0; j <n; j++) {
+		// orderList랑 food.name과 비교해서 같은면 해당 index의 food.price를 합해서 총합을 구한다.
+		for (int j = 0; j <orderList.length; j++) {
 			for (int i = 0; i < food.length; i++) {
-				if(food[i].name == orderList[j]) {
-					total+=price[i];
+				if(food[i].name.equals(orderList[j]) ) {
+					total+=food[i].price;
+
 				}
 			}
 		}
 		return total;
 	}
 
-
-
-
-
-	//
-	//	public String[] orderList(int n, String[] menu) {
-	//		String[] list = new String[n];
-	//
-	//
-	//
-	//		return list; 
-	//	}
-
-
-
-
-
-
-
-
-
 }
+
+
